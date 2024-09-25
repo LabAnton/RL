710783e417339cbd460ada045caddb1d929a9fb4
Anton <anton-sinner@gmx.de>
2024-09-25 17:45:32 +0200

    Beginning DQN

--- DQN/main.py
+++ DQN/main.py
@@ -2,23 +2,47 @@ import gymnasium as gym
 import math
 import time
 import torch as t
+import torch.nn as nn
+import numpy as np
 import matplotlib.pyplot as plt
+import model as model 
 from tqdm import tqdm
 
 t.manual_seed(333)
+device = t.device('cuda' if t.cuda.is_available() else 'cpu')
 
-env = gym.make("Taxi-v3", render_mode = "human")
-env.reset()
+rewards = []
+replay_memory = []
+memory_max = 96000
+runs = 1000
+epsilon = 0.3
+minibatch_size = 32
 
-#Parameters
-train_episodes = 500
-max_steps = 50
 
-#keep track of rewards per run and how much its explores
-training_rewards = []
-exploration_steps = []
+env = gym.make("Pong-v4", render_mode = 'human')
+state = env.reset()[0]
 
-#for now just use a Neural Network to approximate Q-function from the state number
-for episode in tqdm(range(train_episodes)):
+dqn = model.DQN(env).to(device)
+target_dqn = model.DQN(env)
 
-    break
+for run in range(runs):
+    p = t.rand(1).item()
+    next_state, reward, terminated, truncated, info = env.step(action)
+    #Observation is RGB for Pong 3, 210, 160 (C, H, W), using no history yet
+
+    if p < epsilon: 
+        action = env.action_space.sample()
+    else:
+        obs = t.unsqueeze(t.tensor(next_state.astype('float64')), 0)
+        action = dqn(t.permute(obs, (0, 3, 1, 2)).to(device))
+
+    rewards.append(reward)
+    #No done in pong
+    replay_memory.append({'state': s, 'action': a, 'reward': reward, 'next_state': obs})
+    
+    #change to next state
+    state = next_state
+
+def train_replay(replay_memory, minibatch_size):
+    minibatch = np.random.choise(replay_memory, minibatch_size)
+
