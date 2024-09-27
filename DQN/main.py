83a9f049a2d79a27a9d7f9ab1fc1d4da20de58b8
Anton <anton-sinner@gmx.de>
2024-09-27 17:49:29 +0200

    Skeleton for DQN done

--- DQN/main.py
+++ DQN/main.py
@@ -8,41 +8,76 @@ import matplotlib.pyplot as plt
 import model as model 
 from tqdm import tqdm
 
-t.manual_seed(333)
-device = t.device('cuda' if t.cuda.is_available() else 'cpu')
+t.manual_seed(33)
+device = t.device('cpu')
 
 rewards = []
 replay_memory = []
 memory_max = 96000
-runs = 1000
-epsilon = 0.3
+runs = 50 
+epsilon = 0.8 
 minibatch_size = 32
+gamma = 0.9
 
 
 env = gym.make("Pong-v4", render_mode = 'human')
 state = env.reset()[0]
 
+#Network is overparameterized because I get RGB, I have to check later what to do
 dqn = model.DQN(env).to(device)
-target_dqn = model.DQN(env)
+target_dqn = model.DQN(env).to(device)
+loss_fn = nn.MSELoss()
+optimizer = t.optim.SGD(dqn.parameters(), lr = 0.0001, momentum = 0.9)
+
+#Very inefficient and a lot of lines, can write it more beautifully
+def train_replay(replay_memory, minibatch_size):
+
+    states = t.stack([t.tensor(replay_memory[i][0]) for i in np.random.choice(len(replay_memory), minibatch_size)], dim = 0).to(device)
+    action_rewards = t.stack([t.tensor(replay_memory[i][2]) for i in np.random.choice(len(replay_memory), minibatch_size)], dim = 0)
+    next_state = t.stack([t.tensor(replay_memory[i][3]) for i in np.random.choice(len(replay_memory), minibatch_size)], dim = 0)
+    print(action_rewards.shape)
+    pred = dqn(t.permute(states, (0, 3, 1, 2)).to(device))
+    pred_2 = t.tensor(t.max(dqn(t.permute(states, (0, 3, 1, 2))), dim = 1).values)
+    target_pred = target_dqn(t.permute(states, (0, 3, 1, 2)).to(device))
+
+#    print('Pred:', pred)
+#    print(pred_2)
+#    print('Target_pred:', target_pred.shape)
+#    print('Max Target', t.max(target_pred, dim = 1).values)
+#    print('Action Rewards:', action_rewards)
+    
+    optimizer.zero_grad()
+    target = action_rewards + gamma * t.max(target_pred, dim = 1).values
+    loss = loss_fn(pred_2, target)
+    print(loss)
+    loss.backward()
+    optimizer.step()
+#    print('Final Target:', target)
 
 for run in range(runs):
     p = t.rand(1).item()
-    next_state, reward, terminated, truncated, info = env.step(action)
     #Observation is RGB for Pong 3, 210, 160 (C, H, W), using no history yet
 
     if p < epsilon: 
         action = env.action_space.sample()
     else:
-        obs = t.unsqueeze(t.tensor(next_state.astype('float64')), 0)
-        action = dqn(t.permute(obs, (0, 3, 1, 2)).to(device))
+        obs = t.unsqueeze(t.tensor(state.astype('float64')), 0)
+        action = t.argmax(dqn(t.permute(obs, (0, 3, 1, 2)).to(device))).item()
 
+    next_state, reward, terminated, truncated, info = env.step(action)
     rewards.append(reward)
-    #No done in pong
-    replay_memory.append({'state': s, 'action': a, 'reward': reward, 'next_state': obs})
+    #Replay_memory right now is infinite
+    replay_memory.append([state.astype('float64'), action, reward, next_state.astype('float64')])
     
     #change to next state
     state = next_state
 
-def train_replay(replay_memory, minibatch_size):
-    minibatch = np.random.choise(replay_memory, minibatch_size)
+    train_replay(replay_memory, minibatch_size)
+    #Have no Fixed Q-Training implemented
+    
+    if epsilon > 0.01:
+        epsilon -= 1/100
+    
+
+    
 
