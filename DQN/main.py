48da9033083c8b0edd03bf323519c2047a149654
Anton <anton-sinner@gmx.de>
2024-10-09 21:40:47 +0200

    Added Maxpool and Greyscale image

--- DQN/main.py
+++ DQN/main.py
@@ -8,28 +8,31 @@ import matplotlib.pyplot as plt
 import model as model 
 from tqdm import tqdm
 
+t.cuda.empty_cache()
 t.manual_seed(33)
 device = t.device('cuda' if t.cuda.is_available() else 'cpu')
 
 reward_per_game = []
 replay_memory = []
-losses = []
-memory_max = 25600
-games = 300 
+length_game = []
+memory_max = 6000 
+games = 50 
 epsilon = 0.1 
-minibatch_size = 256 
+minibatch_size = 512 
 gamma = 0.9
+C = 20
 
 ##########Things still to do###########
-# 1. Change RGB to Greyscale or increase conv such that last layer is smaller 
+# 1. Change RGB to Greyscale or increase conv such that last layer is smaller -> Done and added Maxpool2d
 # 2. Rewrite train_replay, such that it is better to read and more efficient -> Done 
 # 3. Set-up Fixed-Q target -> Done
-# 4. Try a training run 
+# 4. Try a training run -> Does not learn after 50 games with epsilon 0.1 and C = 20
 # 5. Hyperparameter study
-# 6. Get everything on GPU
+# 6. Get everything on GPU -> Done
 # 7. Have not thought about terminal state yet. -> Done
-# 8. Introduce gradient clipping to stabilize training
+# 8. Introduce gradient clipping to stabilize training -> Not needed since rewards are 1, 0 and -1
 # 9. Look at Huber loss for other games
+# 10. How are histories implemented ?
 
 env = gym.make("Pong-v4", render_mode = None)
 
@@ -42,27 +45,19 @@ loss_fn     = nn.MSELoss()
 optimizer   = t.optim.SGD(dqn.parameters(), lr = 0.00001, momentum = 0.9)
 
 def train_replay(replay_memory, minibatch_size):
+
     random_choice = np.random.choice(len(replay_memory), minibatch_size)
-#    print(np.random.choice(len(replay_memory), minibatch_size))
     states          = t.cat([replay_memory[i][0] for i in random_choice], dim = 0)
     action_rewards  = t.stack([replay_memory[i][1] for i in random_choice], dim = 0)
     next_state      = t.cat([replay_memory[i][2] for i in random_choice], dim = 0)
     terminated      = t.stack([replay_memory[i][3] for i in random_choice], dim = 0)
 
-#    print('States:', states.shape)
-#    print('Next_States:', next_state.shape)
-#    print('Action_rewards:', action_rewards.shape)
-
     main_q_value    = t.max(dqn(states), dim = 1).values
     target_q_value  = t.max(target_dqn(next_state), dim = 1).values
-#    print('Main_q_value:', main_q_value.shape, 'Target_q_value:', target_q_value.shape)
    
     optimizer.zero_grad()
     target = action_rewards + gamma * target_q_value * terminated
-#    print('Target:', target)
-#    print('Terminated:', terminated)
     loss = loss_fn(main_q_value, target)
-    losses.append(loss.item())
     loss.backward()
     optimizer.step()
     
@@ -71,8 +66,10 @@ for game in tqdm(range(games)):
     rewards = []
     terminated   = False
     state = env.reset()[0]
-    
+
     state = t.permute(t.unsqueeze(t.tensor(state.astype('float64')), 0), (0, 3, 1, 2)).to(device)
+    state = t.unsqueeze(t.sum(state/3, 1), 0)
+    n = 0
     while not terminated:
         p = t.rand(1).item()
         #Observation is RGB for Pong 3, 210, 160 (C, H, W), using no history yet
@@ -80,12 +77,12 @@ for game in tqdm(range(games)):
         if p < epsilon: 
             action = env.action_space.sample()
         else:
-            action = t.argmax(dqn(state)).item()
+            action = t.argmax(dqn(state).to(device)).item()
 
         next_state, reward, terminated, truncated, info = env.step(action)
         rewards.append(reward)
 
-        next_state = t.permute(t.unsqueeze(t.tensor(next_state.astype('float64')), 0), (0, 3, 1, 2)).to(device)
+        next_state = t.unsqueeze(t.sum(t.permute(t.unsqueeze(t.tensor(next_state.astype('float64')), 0), (0, 3, 1, 2)).to(device)/3, 1), 0)
         reward = t.tensor(reward).to(device)
         termi = t.tensor(not terminated).to(device)
 
@@ -99,17 +96,21 @@ for game in tqdm(range(games)):
         #Train DQN
         train_replay(replay_memory, minibatch_size)
 
+        #Fixed-update    
+        if n % 20 == 0:
+            target_dqn_state_dict   = target_dqn.state_dict() 
+            dqn_state_dict          = dqn.state_dict()
+            for key in dqn_state_dict:
+                target_dqn_state_dict[key] = dqn_state_dict[key]
+                target_dqn.load_state_dict(target_dqn_state_dict)    
+        n += 1
+
+
 #    print(sum(rewards))
     reward_per_game.append(sum(rewards))
-    
-
-    #Fixed-update    
-    target_dqn_state_dict   = target_dqn.state_dict() 
-    dqn_state_dict          = dqn.state_dict()
-
-    for key in dqn_state_dict:
-        target_dqn_state_dict[key] = dqn_state_dict[key]
-    target_dqn.load_state_dict(target_dqn_state_dict)    
+    length_game.append(len(rewards))
+    # Since Pong only has rarely a negative reward, lets delete all 0 rewards state after the run such that it trains more on the negative expiereneces 
+    replay_memory = list(filter(lambda x: x == t.tensor(0), replay_memory))
 
     if epsilon > 0.01:
         epsilon -= 1/1000
@@ -118,5 +119,5 @@ fig_1, ax_1 = plt.subplots()
 ax_1.plot(np.arange(len(reward_per_game)), reward_per_game, 'x')
 plt.show()
 fig_2, ax_2 = plt.subplots()
-ax_2.plot(np.arange(len(losses)), losses, linewidth = 2)
+ax_2.plot(np.arange(len(length_game)), length_game, 'x')
 plt.show()
--- DQN/model.py
+++ DQN/model.py
@@ -5,16 +5,19 @@ class DQN(nn.Module):
     def __init__(self, env):
         super().__init__()
         
-        self.Conv1 = nn.Conv2d(3, 32, 10, stride = 4) 
-        self.Conv2 = nn.Conv2d(32, 64, 8, stride = 2)
-        self.Conv3 = nn.Conv2d(64, 64, 4, stride = 2)
+        self.Conv1 = nn.Conv2d(1, 16, 8, stride = 4) 
+        self.Pool1 = nn.MaxPool2d(3, stride = 2)
+        self.Conv2 = nn.Conv2d(16, 32, 4, stride = 2)
+        self.Conv3 = nn.Conv2d(32, 64, 2, stride = 1)
         self.flatten = nn.Flatten()
-        self.Linear1 = nn.Linear(4480, 512) 
+        self.Linear1 = nn.Linear(512, 512) 
         self.Linear2 = nn.Linear(512, env.action_space.n)
 
     def forward(self, x):
         x = F.relu(self.Conv1(x))
+        x = self.Pool1(x)
         x = F.relu(self.Conv2(x))
+        x = self.Pool1(x)
         x = F.relu(self.Conv3(x))
         x = self.flatten(x)
         x = F.relu(self.Linear1(x))
